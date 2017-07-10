966ffaea42dc9d1bea1f8fd5d8fecd40e6ae6912
Roman V Kovalev <superturfirma@gmail.com>
2017-07-10 11:09:58 +0400

    Written weblancer parsing code. TODO:make .py as module

--- app/parse_weblancer.py
+++ app/parse_weblancer.py
@@ -1,20 +1,50 @@
-# -*- coding: utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+import requests
+from bs4 import BeautifulSoup
 
-
-import urllib2
-import re
-from BeautifulSoup import BeautifulSoup          # Для обработки HTML\
-
+default_url = "https://www.weblancer.net/jobs/"
 admin_url = 'https://www.weblancer.net/jobs/administrirovanie-sajtov-6/'
+webdev_url = "https://www.weblancer.net/jobs/veb-programmirovanie-i-sajty-3/"
+webdis_url = "https://www.weblancer.net/jobs/veb-dizajn-i-interfejsy-1/"
+dev_url = "https://www.weblancer.net/jobs/programmirovanie-po-i-sistem-2/"
 
-page = urllib2.urlopen(admin_url)
-soup = BeautifulSoup(page)
 
-all_jobs = soup.findAll('div', {'class': 'row'})
+def get_html(url):
+    r = requests.get(url)
+    return r.text
 
-for job in all_jobs:
-	title = job.find('h1')
-	print title
+
+def get_admin_job():
+    admin_job = soup.findAll('div', {'class': 'row'})
+
+
+def main():
+    admin = get_html(admin_url)
+    soup = BeautifulSoup(admin,"html.parser")
+    admin_jobs = soup.find_all("div", class_="row")
+    admin_jobs.pop(0)
+    admin_jobs.pop(0)
+    admin_jobs.pop()
+
+    for admin_job in admin_jobs:
+        job = admin_job.find("h2")
+
+        if job != None:
+            title = job.find("a").text
+            url = default_url + job.find("a").get('href')
+        date = admin_job.find("span", class_="time_ago").get('title')
+        price = admin_job.find("div",class_="amount").text.strip()
+        content = admin_job.find("div", style="margin-top: -10px; margin-bottom: -10px")
+        if content == None:
+            content = 'Смотри описание по ссылке'
+        else:
+            content = content.text
+        print(title)
+        print(url)
+        print(date)
+        print(price)
+        print(content)
+        print("-----------------------------------------------------------------------")
+
+
+if __name__ == '__main__':
+    main()
