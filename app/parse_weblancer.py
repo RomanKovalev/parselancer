65a4005815ad23771bc1e61cb9e6730284c3b449
Kovalev Roman <superturfirma@gmail.com>
2017-09-24 19:35:44 +0300

    Small changes in var names. Del custom .gitignore

--- app/parse_weblancer.py
+++ app/parse_weblancer.py
@@ -13,28 +13,24 @@ def get_html(url):
     return r.text
 
 
-# def get_admin_job():
-#     admin_job = soup.findAll('div', {'class': 'row'})
-
 def parse_category(url, category):
-    admin = get_html(url)
-    soup = BeautifulSoup(admin, "html.parser")
-    admin_jobs = soup.find_all("div", class_="row")
-    admin_jobs.pop(0)
-    admin_jobs.pop(0)
-    admin_jobs.pop()
+    page = get_html(url)
+    soup = BeautifulSoup(page, "html.parser")
+    jobs = soup.find_all("div", class_="row")
+    jobs.pop(0)
+    jobs.pop(0)
+    jobs.pop()
 
-    for admin_job in admin_jobs:
-        job = admin_job.find("h2")
+    for job in jobs:
+        the_job = job.find("h2")
 
         if job != None:
             title = job.find("a").text
             url = default_url + job.find("a").get('href')
 
-        date = admin_job.find("span", class_="time_ago").get('title')
-        price = admin_job.find("div", class_="amount").text.strip()
-        content = admin_job.find("div", style="margin-top: -10px; margin-bottom: -10px")
-        category = category
+        date = job.find("span", class_="time_ago").get('title')
+        price = job.find("div", class_="amount").text.strip()
+        content = job.find("div", style="margin-top: -10px; margin-bottom: -10px")
         if content == None:
             content = 'Смотри описание по ссылке'
         else:
